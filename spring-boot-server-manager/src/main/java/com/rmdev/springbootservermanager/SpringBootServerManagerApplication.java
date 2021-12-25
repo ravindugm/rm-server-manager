7a422e16ccfc34a12aa62eb26c387e1848afbbe7
ravindugm <ravindumiyuranga@gmail.com>
2021-12-25 23:43:48 +0530

    Initial commit for backend

---
+++ spring-boot-server-manager/src/main/java/com/rmdev/springbootservermanager/SpringBootServerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rmdev.springbootservermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServerManagerApplication.class, args);
+	}
+
+}
